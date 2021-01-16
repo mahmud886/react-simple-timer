9f1c6a53ef643c2e4373fc0da07bca7ed2d361d8
mahmud886 <iqbal886mahmud@gmail.com>
2021-01-16 20:23:53 +0600

    all files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		count: 0,
+	};
+
+	intervalId = null;
+
+	increamentCount = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+	decrementCount = () => {
+		if (this.state.count > 0) {
+			this.setState({ count: this.this.state - 1 });
+		}
+	};
+
+	startTimer = () => {
+		if (this.state.count > 0 && !this.intervalId) {
+			this.intervalId = setInterval(() => {
+				this.setState({ count: this.state.count - 1 }, () => {
+					if (this.state.count === 0) {
+						alert('Timer finished');
+						clearInterval(this.intervalId);
+						this.intervalId = null;
+					}
+				});
+			}, 1000);
+		}
+	};
+
+	stopTimer = () => {
+		if (this.intervalId) {
+			clearInterval(this.intervalId);
+			this.intervalId = null;
+		}
+	};
+
+	resetTimer = () => {
+		this.setState({ count: 0 });
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="App">
+					<h1 className="Heading">Simple Timer</h1>
+					<div className="Container">
+						<button className="Btn" onClick={this.increamentCount}>
+							-
+						</button>
+						<span className="Text">{this.state.count}</span>
+						<button className="Btn" onClick={this.increamentCount}>
+							+
+						</button>
+					</div>
+					<div className="Container">
+						<button className="Btn" onClick={this.startTimer}>
+							Start
+						</button>
+						<button className="Btn" onClick={this.stopTimer}>
+							Stop
+						</button>
+						<button className="Btn" onClick={this.resetTimer}>
+							Reset
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
